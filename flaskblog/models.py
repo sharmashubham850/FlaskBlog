4740e15a725d529c7f94133a0d6fc60365de45a6
sharmashubham850 <sharmashubham850@gmail.com>
2019-09-26 15:53:28 +0530

    Added User account page and Account update form

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -14,7 +14,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default="profile.jpg")
+    image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
 
     posts = db.relationship("Post", backref="author", lazy=True)
 
